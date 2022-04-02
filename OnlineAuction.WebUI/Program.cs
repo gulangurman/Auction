b5398cabe0a692fc514c33b6bad6b0bcffa5171b
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-04-02 10:58:18 +0300

    Added Web UI project.

---
+++ OnlineAuction.WebUI/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
