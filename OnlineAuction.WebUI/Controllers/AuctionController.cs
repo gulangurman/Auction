494fdcd536ae4c97fe2971027b1485dc7809a9e4
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-04-02 16:18:15 +0300

    Added auction MVC.

---
+++ OnlineAuction.WebUI/Controllers/AuctionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineAuction.WebUI.Controllers
+{
+    [Authorize]
+    public class AuctionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineAuction.WebUI/Models/AuctionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineAuction.WebUI.Models
+{
+    public class AuctionViewModel
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter product")]
+        public string ProductId { get; set; }
+        [Required(ErrorMessage = "Please enter quantity")]
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [Required(ErrorMessage = "Please enter qustart date")]
+        public DateTime StartedAt { get; set; }
+        [Required(ErrorMessage = "Please enter finish date")]
+        public DateTime FinishedAt { get; set; }
+        public int Status { get; set; }
+        public int SellerId { get; set; }
+        public List<string> IncludedSellers { get; set; }
+    }
+}
