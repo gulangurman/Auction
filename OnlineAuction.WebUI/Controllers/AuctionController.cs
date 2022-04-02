c99bae9196223adfa514d6a727e0dba874a7b608
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-04-02 17:44:02 +0300

    Added action methods and views for auction operations.

--- OnlineAuction.WebUI/Controllers/AuctionController.cs
+++ OnlineAuction.WebUI/Controllers/AuctionController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineAuction.WebUI.Models;
 
 namespace OnlineAuction.WebUI.Controllers
 {
     [Authorize]
     public class AuctionController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuctionController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            ViewBag.Users = users;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AuctionViewModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Details()
         {
             return View();
         }
