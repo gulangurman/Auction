f46aa8bc0526b2974197201340a7c02c8ea81478
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-04-02 11:24:08 +0300

    Added EF Core Identity support.

---
+++ OnlineAuction.WebUI/Data/WebAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineAuction.WebUI.Models;
+
+namespace OnlineAuction.WebUI.Data
+{
+    public class WebAppContext : IdentityDbContext<AppUser>
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+
+    }
+}
